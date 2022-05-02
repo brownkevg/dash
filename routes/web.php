41a6d7495015909d2df9ef231c599be125b15c90
brownkevg <brownkevg@gmail.com>
2022-05-02 15:11:06 -0700

    adding dependency and updating php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/user', function () {
 	return App\Models\User::first();
 });
